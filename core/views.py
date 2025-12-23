c61e486dba114f05f2705daac24e6cdb1c4e9220
Ratchanon Moungwichean <mynameisnon23@gmail.com>
2025-12-24 00:44:58 +0700

    add homepage

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Audio
 
 # Create your views here.
+def index(request):
+    audios = Audio.objects.all()
+    return render(request, 'index.html', {'audios': audios})
