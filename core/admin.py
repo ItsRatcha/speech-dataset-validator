e4f8b54dbbd0c877e2810c6f41aa5bb8ed07c0e9
Ratchanon Moungwichean <mynameisnon23@gmail.com>
2025-12-23 23:58:41 +0700

    add models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Lang, Audio, Rating
 
 # Register your models here.
+admin.site.register(Lang)
+admin.site.register(Audio)
+admin.site.register(Rating)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Lang(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Audio(models.Model):
+    id = models.AutoField(primary_key=True)
+    lang = models.ForeignKey(Lang, on_delete=models.CASCADE)
+    audio = models.URLField()
+    transcript = models.TextField()
+
+    def __str__(self):
+        return f"Audio {self.id} - {self.lang.name}"
+
+
+class Rating(models.Model):
+    id = models.AutoField(primary_key=True)
+    uid = models.ForeignKey(User, on_delete=models.CASCADE)
+    pronounciation_score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    naturalness_score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    noise_score = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    edit_transcript = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Rating {self.id} - User {self.uid.username}"
